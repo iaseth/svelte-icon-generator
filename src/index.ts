2d9687ca99a31acd46624c183debf5be379db7e1
Ankur Seth <iankurseth@gmail.com>
2025-04-26 17:27:30 +0530

    Importing json using fs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
 
-console.log("Hello, svig!");
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+interface Icon {
+	title: string,
+	svg: string
+}
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const iconsJson = JSON.parse(
+	fs.readFileSync(path.join(__dirname, 'icons.json'), 'utf-8')
+);
+
+const icons: Icon[] = iconsJson.icons;
+
+for (const icon of icons) {
+	console.log(`${icon.title} (${icon.svg.length}b)`);
+}
