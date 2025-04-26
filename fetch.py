1b027f70960a0a70bc7ad17d077206b430fed7cf
Ankur Seth <iankurseth@gmail.com>
2025-04-26 14:48:43 +0530

    Added script to download all icons

---
+++ fetch.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def main():
+	response = requests.get("https://heroicons.com/")
+	soup = BeautifulSoup(response.text, "lxml")
+
+	main = soup.find("main")
+	groups = main.find_all('div', class_="group")
+
+	icons = []
+	for group in groups:
+		svg = group.find("svg")
+		if svg:
+			icon = {}
+			icon['title'] = list(group.children)[-1]['title']
+			icon['svg'] = str(svg)
+			icons.append(icon)
+
+	for i, icon in enumerate(icons, start=1):
+		svg_path = f"svgs/{icon['title']}.svg"
+		print(f"{i:4}. '{icon['title']}'")
+		with open(svg_path, "w") as f:
+			f.write(icon['svg'])
+		print(f"\tSaved: {svg_path}")
+
+
+if __name__ == '__main__':
+	main()
