5f314cc0fb8f3eaf0ee7af3d32d35078068990a2
Ankur Seth <iankurseth@gmail.com>
2025-04-27 13:36:04 +0530

    Moved commands into commands.ts

---
+++ src/commands.ts
@@ -0,0 +1,76 @@
+import { IconDS, deleteSvelteComponentOnDisk, generateSvelteComponentOnDisk, icons } from './icon.js';
+import { getSvigConfig, saveSvigConfig, svigConfigPath } from './config.js';
+
+
+
+export function addIconsCommand (iconNames: string[], dirpath: string) {
+	if (iconNames.length === 0) {
+		console.log(`Please provide icons to add!`); return;
+	}
+
+	const config = getSvigConfig();
+	for (const iconName of iconNames) {
+		const icon = icons.find(ic => ic.name === iconName);
+		if (icon) {
+			if (config.icons.includes(iconName)) {
+				console.log(`Icon already added: '${iconName}'`);
+				generateSvelteComponentOnDisk(icon, dirpath, false);
+			} else {
+				console.log(`Icon was added: '${iconName}'`);
+				config.icons.push(iconName);
+				saveSvigConfig(config);
+				generateSvelteComponentOnDisk(icon, dirpath, true);
+			}
+		} else {
+			console.log(`Icon not found: '${iconName}'`);
+		}
+	}
+}
+
+export function removeIconsCommand (iconNames: string[], dirpath: string) {
+	if (iconNames.length === 0) {
+		console.log(`Please provide icons to remove!`); return;
+	}
+
+	const config = getSvigConfig();
+	if (!config.icons || config.icons.length === 0) {
+		console.log(`No icons found in config: '${svigConfigPath}'`);
+		return;
+	}
+
+	for (const iconName of iconNames) {
+		if (config.icons.includes(iconName)) {
+			config.icons = config.icons.filter(ic => ic !== iconName);
+			saveSvigConfig(config);
+			console.log(`Icon was removed: '${iconName}'`);
+		} else {
+			console.log(`Icon not present in config: '${iconName}'`);
+		}
+		deleteSvelteComponentOnDisk(iconName, dirpath);
+	}
+}
+
+export function generateIconsCommand (dirpath: string) {
+	const config = getSvigConfig();
+	if (config.icons.length === 0) {
+		console.log(`No icond found in config: '${svigConfigPath}'`)
+		return;
+	}
+
+	console.log(`Generating ${config.icons.length} component(s) . . .`)
+	for (const iconName of config.icons) {
+		const icon = icons.find(icon => icon.name === iconName);
+		if (icon) {
+			generateSvelteComponentOnDisk(icon, dirpath, true);
+		} else {
+			console.log(`\tIcon not found: '${iconName}'`);
+		}
+	}
+}
+
+export function listIconsCommand (iconArr: IconDS[]) {
+	for (let i=0; i < iconArr.length; i++) {
+		const icon = iconArr[i];
+		console.log(`Icon ${i+1} => '${icon.name}'`);
+	}
+}
