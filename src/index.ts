bed9b4ae13d3672c5ed306195b64e7b5bbb8e118
Ankur Seth <iankurseth@gmail.com>
2025-04-27 09:22:29 +0530

    Now using argparse

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,58 @@
 #!/usr/bin/env node
 
 import * as fs from 'fs';
+import { ArgumentParser } from 'argparse';
 import { getSvelteFileName } from './utils.js';
 import { generateSvelteComponent, icons } from './icon.js';
 
 
 
-function main() {
-	const args = process.argv.slice(2);
-	if (args.length === 0) {
-		console.log("Usage: svig icon-one icon-two . . .")
-		return;
-	}
-	
-	for (const arg of args) {
-		const icon = icons.find(icon => icon.title === arg);
+function generateIcons (iconNames: string[]) {
+	for (const iconName of iconNames) {
+		const icon = icons.find(icon => icon.title === iconName);
 		if (icon) {
 			const filename = getSvelteFileName(icon.title);
 			const content = generateSvelteComponent(icon.svg);
 			fs.writeFileSync(filename, content);
 			console.log(`Generated: '${filename}'`);
 		} else {
-			console.log(`Icon not found: '${arg}'`);
+			console.log(`Icon not found: '${iconName}'`);
 		}
+	}
+}
+
+function main() {
+	const parser = new ArgumentParser({
+		description: 'svig - Command-line tool for generating Svelte Icon Components',
+	});
+
+	// Define arguments
+	parser.add_argument('command', {
+		help: 'The command to run'
+	});
+	parser.add_argument('-o', '--out', {
+		help: 'Output directory path',
+		metavar: 'DIR',
+		default: "src/lib/components/svig"
+	});
+	parser.add_argument('-s', '--save', {
+		action: 'store_true',
+		help: 'Save to disk',
+		default: false
+	});
+	const [args, rest] = parser.parse_known_args();
+
+	switch (args.command) {
+		case "generate":
+			if (rest.length === 0) {
+				console.log("Usage: svig generate icon-one icon-two . . .")
+				return;
+			} else {
+				generateIcons(rest);
+			}
+			break;
+		default:
+			console.log(`Unknown command: '${args.command}'`); return;
 	}
 }
 
