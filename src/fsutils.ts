1add79010c4b2f90830a6aa3cbc5bd24479ebb94
Ankur Seth <iankurseth@gmail.com>
2025-04-27 08:30:40 +0530

    Now using handlebars

---
+++ src/fsutils.ts
@@ -0,0 +1,12 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export function readFile (filepath: string): string {
+	return fs.readFileSync(path.join(__dirname, filepath), 'utf-8');
+}
--- src/icon.ts
+++ src/icon.ts
@@ -1,6 +1,5 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import { fileURLToPath } from 'url';
+import Handlebars from "handlebars";
+import { readFile } from "./fsutils.js";
 
 
 
@@ -9,33 +8,12 @@ export interface Icon {
 	svg: string
 }
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-function readFile (filepath: string): string {
-	return fs.readFileSync(path.join(__dirname, filepath), 'utf-8');
-}
-
 const iconsJson = JSON.parse(readFile('data/icons.json'));
 
 export const icons: Icon[] = iconsJson.icons;
 
 export function generateSvelteComponent(svgContent: string): string {
-	const template = readFile('templates/SvelteComponent.html');
-	console.log(template);
-	svgContent = svgContent.trim();
-	return `
-<script lang="ts">
-	interface Props {
-		size?: number,
-		class?: string
-	}
-	let { size, ...props }: Props = $props();
-	let px = \`\${size || 160}px\`;
-</script>
-
-<div style:height={px} style:width={px} class={props.class || ""}>
-	${svgContent}
-</div>
-`;
+	const templateSrc = readFile('templates/SvelteComponent.hbs');
+	const template = Handlebars.compile(templateSrc);
+	return template({ svgContent });
 }
